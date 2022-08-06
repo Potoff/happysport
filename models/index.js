22ebdde720f45ae92992f94e5488998e5f6c07d4
Paul Demarest <services.ankaamusic@gmail.com>
2022-08-06 22:55:35 +0200

    add sequelize and connect to postgres

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(path.join(__dirname, '../config/config'))[env];
 const db = {};
 
 let sequelize;
@@ -14,6 +14,12 @@ if (config.use_env_variable) {
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
+sequelize.sync({force: true})
+  .then( () => console.log('DB SYNC'))
+  .catch((err) => {
+    console.log({err: err})
+  });
 
 fs
   .readdirSync(__dirname)
